08f8d32ac42ed14be939b8b4abd8a09ff5a22633
Narias1999 <nicolasarias870@gmail.com>
2018-04-03 08:19:50 -0500

    initial comit

---
+++ database/migrations/2018_04_03_034059_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('user')->unsigned();
+            $table->integer('post')->unsigned();
+            $table->text('comment');
+            $table->foreign('user')->references('id')->on('users');
+            $table->foreign('post')->references('id')->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
