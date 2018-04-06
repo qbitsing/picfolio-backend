4d72b19bf89cc98f88ff918dce8a49c7d96f8700
Narias1999 <nicolasarias870@gmail.com>
2018-04-05 21:11:38 -0500

    solving comment error

--- app/Comment.php
+++ app/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    protected $fillable = ['comment'];
+    protected $fillable = ['comment', 'user_id', 'post_id'];
     protected $hidden = ['post_id', 'user_id', 'updated_at'];
     public function user(){
         return $this->belongsTo('App\User');
--- app/Post.php
+++ app/Post.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['user_id', 'image', 'likes', 'id', 'description'];
+    protected $attributes = [
+        'likes' => 0
+    ];
     public function comments() {
         return $this->hasMany('App\Comment');
     }
